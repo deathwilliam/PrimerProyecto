a5fca6c69d6ce6ba0d2dc479ed3aecbd486889cd
SrDiefer <94631654+SrDiefer@users.noreply.github.com>
2023-03-10 14:29:54 -0300

    revision
    agrego codigo

--- app.js
+++ app.js
@@ -4,3 +4,8 @@
 
 #....
 #...
+
+
+# Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit consequuntur, laudantium consectetur molestias id accusantium rem voluptate animi cupiditate perferendis ipsa similique dolor ut praesentium suscipit, officia iusto asperiores hic.
+
+let objeto = 500;
